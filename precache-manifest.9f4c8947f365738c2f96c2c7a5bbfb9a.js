051ca91d95c97b3d7f7461c21df67ee7a0397238
Drawmet <drawmet.vladyslav@gmail.com>
2019-02-19 16:56:47 +0200

    Updates

--- precache-manifest.9f4c8947f365738c2f96c2c7a5bbfb9a.js
+++ precache-manifest.9f4c8947f365738c2f96c2c7a5bbfb9a.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "6092b2de6eaba1bbc52d",
+    "revision": "ddb9be6f88a8aa500bd3",
     "url": "/tmdb-app/static/css/main.0a7dd405.chunk.css"
   },
   {
-    "revision": "6092b2de6eaba1bbc52d",
-    "url": "/tmdb-app/static/js/main.6092b2de.chunk.js"
+    "revision": "ddb9be6f88a8aa500bd3",
+    "url": "/tmdb-app/static/js/main.ddb9be6f.chunk.js"
   },
   {
     "revision": "a22abbad16ad957678ed",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/tmdb-app/static/js/2.36912be8.chunk.js"
   },
   {
-    "revision": "ee14e2d0613150f0a223cc74524b8781",
+    "revision": "b44dee8d63d89ad99fc9e96bcf5e3d9c",
     "url": "/tmdb-app/index.html"
   }
 ];
